1d7683b22524cd8abae2b9e9bc95f491df21fb7d
Joseph Monakedi <jimonakedi@gmail.com>
2024-11-14 00:34:41 +0200

    style show previewcards

--- src/components/preview-window.tsx
+++ src/components/preview-window.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect} from "react";
+import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent  } from "./ui/card";
 
 interface PodcastPreview {
   id: number
@@ -21,11 +22,26 @@ export default function PodcastPreviewList() {
             <h1>Podcast Preview List</h1>
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                 {podcasts.map((podcast) => (
-                    <div key={podcast.id} className="flex flex-col p-4 border rounded-lg shadow-md">
-                        <h2 className="font-bold">{podcast.title}</h2>
-                        <p className="mt-2 text-sm">{podcast.description}</p>
-                        <img src={podcast.image} alt={podcast.title} className="w-full mt-4" />
-                    </div>
+                    <Card 
+                    key={podcast.id} 
+                    style={
+                        {backgroundImage: `url(${podcast.image})`,
+                        backgroundSize: "100% 100%",
+                         
+                        aspectRatio: "1/1",}
+                    }>  
+                        <button className="h-full">
+                        <CardContent className="relative top-0 inset-0 flex flex-col place-content-between h-full bg-white bg-opacity-85 opacity-0 transition-opacity duration-500 hover:opacity-100">
+                            <CardHeader className="h-4/5">
+                                <CardTitle>{podcast.title}</CardTitle>
+                                <CardDescription className="text-current overflow-y-auto">{podcast.description}</CardDescription>
+                            </CardHeader>  
+                            <CardFooter>
+                                <p>Seasons: {podcast.seasons}</p>
+                            </CardFooter>
+                        </CardContent>
+                        </button>
+                    </Card>
                 ))}
             </div>
         </div>
