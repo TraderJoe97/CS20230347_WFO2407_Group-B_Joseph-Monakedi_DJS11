da1d57d7dbb1ffd911383017e9d79a16ff53df7c
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2024-11-20 14:56:17 +0200

    get showid from useParams

--- src/pages/PodcastDetails.tsx
+++ src/pages/PodcastDetails.tsx
@@ -1,30 +1,31 @@
 // import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 export default function PodcastDetails() {
-    const { params } = useParams();
-    console.log(params);
-    interface show {
-        id: number,
-        title: string,
-        description: string,
-        seasons: Array<season>,
-    }
+    const params = useParams();
+    const showId = params.id
 
-    interface season {
-        id: number,
-        title: string,
-        image: string,
-        episodes: Array<episode>,
-    }
+    // interface show {
+    //     id: number,
+    //     title: string,
+    //     description: string,
+    //     seasons: Array<season>,
+    // }
+
+    // interface season {
+    //     id: number,
+    //     title: string,
+    //     image: string,
+    //     episodes: Array<episode>,
+    // }
     
-    interface episode {
-        id: number,
-        title: string,
-        file: string,
-    }
+    // interface episode {
+    //     id: number,
+    //     title: string,
+    //     file: string,
+    // }
     
 
     return (
-        <div>hello</div>
+        <div>`${showId}`</div>
     )
 }
