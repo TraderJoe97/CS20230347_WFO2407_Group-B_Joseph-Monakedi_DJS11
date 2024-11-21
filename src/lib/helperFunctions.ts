2ffffd811059af9c7a14c08c3d34ea9e6b409df6
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2024-11-21 16:54:12 +0200

    refactor formatTime helperfunction

--- src/lib/helperFunctions.ts
+++ src/lib/helperFunctions.ts
@@ -1,4 +1,14 @@
-export const formatTime = (timeInSeconds) => {
+/**
+ * Format a time in seconds into a string in the format 'HH:MM:SS'
+ *
+ * @param {number | null | undefined} timeInSeconds - The time in seconds to be formatted
+ * @returns {string} - The formatted time string
+ */
+export const formatTime = (timeInSeconds: number | null | undefined): string => {
+    if (timeInSeconds === null || timeInSeconds === undefined) {
+        return '00:00:00';
+    }
+
     const hours = Math.floor(timeInSeconds / 3600);
     const minutes = Math.floor((timeInSeconds % 3600) / 60);
     const seconds = Math.floor(timeInSeconds % 60);
