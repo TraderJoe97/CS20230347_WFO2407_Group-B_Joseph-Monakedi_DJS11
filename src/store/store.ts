3a4aead9425cd8da7c1bb78d5a723481b068b8dd
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2024-11-22 17:03:10 +0200

    addFav slice to global state store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import playerReducer from "./playerSlice";
+import favouritesReducer from "./FavouritesSlice";
 
+const rootReducer = combineReducers({
+    player: playerReducer,
+    favourites: favouritesReducer
+});
 
 export const store = configureStore({
-    reducer: {
-        player: playerReducer
-    }
+    reducer: rootReducer
 });
 
 export type RootState = ReturnType<typeof store.getState>;
